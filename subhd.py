4461a5eb59a5fa0424235a9baf3c40b94c5ccd2f
pa4373 <pa4373@gmail.com>
2015-04-01 06:36:58 +0800

    Remove test code in subhd.py

--- subhd.py
+++ subhd.py
@@ -1,6 +1,5 @@
 #/usr/bin/env python
 import core
+
 if __name__ == '__main__':
-    downloader = core.SubHDDownloader()
-    t = downloader.search('Person of Interest', False)
-    print t
+    pass
